052295ada02201abe559a63fe2cc97830b1becee
Anderson Rivera <anderson.pr.dev@gmail.com>
2025-02-02 13:56:33 -0500

    add category controller

--- src/main/java/com/anderson/cityshop/controller/CategoryRestController.java
+++ src/main/java/com/anderson/cityshop/controller/CategoryRestController.java
@@ -18,18 +18,64 @@ public class CategoryRestController {
         this.categoryService = categoryService;
     }
 
+    /**
+     * Searches for all categories.
+     *
+     * @return a ResponseEntity containing the CategoryResponseRest object,
+     *         which includes a list of all available categories and related metadata
+     */
     @GetMapping("/categories")
     public ResponseEntity<CategoryResponseRest> searchCategories() {
         return categoryService.search();
     }
 
+    /**
+     * Saves a new category to the system.
+     *
+     * @param category the category object containing the details to be saved
+     * @return a ResponseEntity containing the CategoryResponseRest object,
+     *         which includes the saved category details and metadata
+     */
     @PostMapping("/categories")
     public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category) {
         return categoryService.save(category);
     }
-    
+
+    /**
+     * Searches for a category by its ID.
+     *
+     * @param id the unique identifier of the category to search for
+     * @return a ResponseEntity containing the CategoryResponseRest object,
+     *         which includes the details of the requested category
+     */
     @GetMapping("/categories/{id}")
 	public ResponseEntity<CategoryResponseRest> searchCategoriesById(@PathVariable Long id) {
 		return categoryService.searchById(id);
 	}
+
+    /**
+     * Updates an existing category in the system.
+     *
+     * @param category the details of the category to be updated
+     * @param id the unique identifier of the category to be updated
+     * @return a ResponseEntity containing the CategoryResponseRest object,
+     *         which includes the updated category details and metadata
+     */
+    @PutMapping("/categories/{id}")
+    public ResponseEntity<CategoryResponseRest> update(@RequestBody Category category, @PathVariable Long id) {
+        return categoryService.update(category, id);
+    }
+
+    /**
+     * Deletes a category identified by its unique ID.
+     *
+     * @param id the unique identifier of the category to be deleted
+     * @return a ResponseEntity containing the CategoryResponseRest object,
+     *         which provides information about the success or failure of the deletion operation
+     */
+    @DeleteMapping("/categories/{id}")
+    public ResponseEntity<CategoryResponseRest> deleteById(@PathVariable Long id) {
+        return categoryService.deleteById(id);
+    }
+
 }
--- src/main/java/com/anderson/cityshop/service/CategoryServiceImpl.java
+++ src/main/java/com/anderson/cityshop/service/CategoryServiceImpl.java
@@ -17,10 +17,21 @@ import java.util.Optional;
 @Service
 public class CategoryServiceImpl implements ICategoryService {
 
-    @Autowired
-    private ICategoryDao categoryDao;
+    private final ICategoryDao categoryDao;
 
+    public CategoryServiceImpl(ICategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
+    }
 
+    /**
+     * Searches and retrieves all categories from the database.
+     * Constructs a response containing the list of categories and metadata
+     * about the request status. Handles exceptions and returns an error response
+     * in case of failures.
+     *
+     * @return a ResponseEntity containing a CategoryResponseRest object
+     *         with the list of categories and relevant metadata.
+     */
     @Override
     @Transactional(readOnly = true)
     public ResponseEntity<CategoryResponseRest> search() {
@@ -37,6 +48,13 @@ public class CategoryServiceImpl implements ICategoryService {
         return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
     }
 
+    /**
+     * Searches for a category by its unique identifier (ID) in the database.
+     *
+     * @param id the unique identifier of the category to search for
+     * @return a ResponseEntity containing a CategoryResponseRest object with the found category
+     *         and additional metadata.
+     */
     @Override
     @Transactional(readOnly = true)
 	public ResponseEntity<CategoryResponseRest> searchById(Long id) {
@@ -50,14 +68,14 @@ public class CategoryServiceImpl implements ICategoryService {
 			if(category.isPresent()) {
 				list.add(category.get());
 				response.getCategoryResponse().setCategory(list);
-				response.setMetadata("Respuesta ok", "00", "Categoria encontrada");
+				response.setMetadata("Ok status", "00", "Success response");
 			} else {
-				response.setMetadata("Bad response", "-1", "Categoria no encontrada");
+				response.setMetadata("Bad response", "-1", "Category not found");
 				return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.NOT_FOUND);
 			}
 			
 		} catch (Exception e) {
-			response.setMetadata("Bad response", "-1", "Error al consultar por id");
+			response.setMetadata("Bad response", "-1", "There was an error");
 			e.getStackTrace();
 			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
@@ -65,7 +83,12 @@ public class CategoryServiceImpl implements ICategoryService {
 		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
 	}
 
-
+    /**
+     * Saves a category in the database.
+     *
+     * @param category the category object to be saved
+     * @return a ResponseEntity containing the CategoryResponseRest object and the corresponding HTTP status
+     */
     @Override
     @Transactional
     public ResponseEntity<CategoryResponseRest> save(Category category) {
@@ -77,27 +100,90 @@ public class CategoryServiceImpl implements ICategoryService {
             if(categorySaved != null) {
                 list.add(categorySaved);
                 response.getCategoryResponse().setCategory(list);
-                response.setMetadata("Respuesta ok", "00", "Categoria guardada");
+                response.setMetadata("Ok status", "00", "Category saved");
             } else {
-                response.setMetadata("Bad response", "-1", "Categoria no guardada");
+                response.setMetadata("Bad response", "-1", "Category not saved");
                 return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.BAD_REQUEST);
             }
 
         } catch (Exception e) {
-            response.setMetadata("Bad response", "-1", "Error al crear categoria");
+            response.setMetadata("Bad response", "-1", "There was an error");
             e.getStackTrace();
             return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
     }
 
+    /**
+     * Updates an existing category based on the provided category object and ID.
+     *
+     * @param category the category object containing the updated information
+     * @param id the ID of the category to be updated
+     * @return a ResponseEntity containing the CategoryResponseRest object with the update status
+     */
     @Override
+    @Transactional
     public ResponseEntity<CategoryResponseRest> update(Category category, Long id) {
-        return null;
+        CategoryResponseRest response = new CategoryResponseRest();
+        List<Category> list = new ArrayList<>();
+
+        try {
+
+            Optional<Category> categorySearch = categoryDao.findById(id);
+
+            if (categorySearch.isPresent()) {
+                // update doc
+                categorySearch.get().setName(category.getName());
+                categorySearch.get().setDescription(category.getDescription());
+
+                Category categoryToUpdate = categoryDao.save(categorySearch.get());
+
+                if (categoryToUpdate != null) {
+                    list.add(categoryToUpdate);
+                    response.getCategoryResponse().setCategory(list);
+                    response.setMetadata("Ok status", "00", "Category updated");
+                } else {
+                    response.setMetadata("Bad response", "-1", "Category not updated");
+                    return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.BAD_REQUEST);
+                }
+
+            } else {
+                // send error to user
+                response.setMetadata("Bad response", "-1", "Category not found");
+                return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.NOT_FOUND);
+            }
+
+        } catch (Exception e) {
+            response.setMetadata("Bad response", "-1", "There was an error updating the category");
+            e.getStackTrace();
+            return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
     }
 
+    /**
+     * Deletes a category by its identifier.
+     *
+     * @param id the identifier of the category to be deleted
+     * @return a ResponseEntity containing the CategoryResponseRest object with
+     *         metadata indicating success or failure of the deletion operation
+     */
     @Override
     public ResponseEntity<CategoryResponseRest> deleteById(Long id) {
-        return null;
+        CategoryResponseRest response = new CategoryResponseRest();
+
+        try {
+
+            categoryDao.deleteById(id);
+            response.setMetadata("Ok status", "00", "Category deleted");
+
+        } catch (Exception e) {
+            response.setMetadata("Bad response", "-1", "There was an error deleting the category");
+            e.getStackTrace();
+            return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
     }
 }
